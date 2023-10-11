9df458cc34906f2b7b51342065e80dcb33499b9e
ruan <venancioruan11@gmail.com>
2023-10-10 21:22:19 -0300

    grandes alterações e implementações

--- ERG/sul/views.py
+++ ERG/sul/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    return render(request, 'sul/index.html')
+
+def sc(request):
+    return render(request, 'sul/paises/santa_catarina.html')
